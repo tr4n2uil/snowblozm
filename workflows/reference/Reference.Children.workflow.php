fcd8e19aa1e392870862b6db68486e58087e7e70
Vibhaj Rajan <vibhaj8@gmail.com>
2011-07-28 21:45:38 +0530

    v0.4.1 : added reference.children and reference.parents workflows

---
+++ workflows/reference/Reference.Children.workflow.php
@@ -0,0 +1,57 @@
+<?php 
+require_once(SBSERVICE);
+
+/**
+ *	@class ReferenceChildrenWorkflow
+ *	@desc Manages children listing of existing reference 
+ *
+ *	@param keyid long int Usage Key ID [memory]
+ *	@param id long int Reference ID [memory]
+ *
+ *	@return children array Chain children information [memory]
+ *
+ *	@author Vibhaj Rajan <vibhaj8@gmail.com>
+ *
+**/
+class ReferenceChildrenWorkflow implements Service {
+	
+	/**
+	 *	@interface Service
+	**/
+	public function input(){
+		return array(
+			'required' => array('keyid', 'id')
+		);
+	}
+	
+	/**
+	 *	@interface Service
+	**/
+	public function run($memory){
+		$kernel = new WorkflowKernel();
+		
+		$memory['msg'] = 'Reference children listed successfully';
+		
+		$workflow = array(
+		array(
+			'service' => 'sb.reference.authorize.workflow',
+			'input' => array('chainid' => 'id')
+		),
+		array(
+			'service' => 'sb.web.children.workflow',
+			'input' => array('parent' => 'id')
+		));
+		
+		return $kernel->execute($workflow, $memory);
+	}
+	
+	/**
+	 *	@interface Service
+	**/
+	public function output(){
+		return array('children');
+	}
+	
+}
+
+?>
